60707b505ee8f935b72f936ae31937d13bef24e3
thisellie <81550623+arnplsrz@users.noreply.github.com>
2025-08-01 13:03:32 +0800

    Initial backend structure

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const session = require('express-session')
+const passport = require('passport')
+const { PrismaClient } = require('@prisma/client')
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store')
+require('dotenv').config()
+require('./config/passport')
+
+const authRoutes = require('./routes/auth')
+
+const app = express()
+const prisma = new PrismaClient()
+
+app.set('view engine', 'ejs')
+app.set('views', `${__dirname}/views`)
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(passport.session())
+
+app.use(
+  session({
+    cookie: { maxAge: 24 * 60 * 60 * 1000 },
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 2 * 60 * 1000,
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+)
+
+app.use('/', authRoutes)
+
+app.listen(process.env.PORT)
