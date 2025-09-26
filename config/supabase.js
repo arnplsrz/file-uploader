1902c28ea55c52aa04c21f010d929dcb6d0b3ab8
thisellie <81550623+arnplsrz@users.noreply.github.com>
2025-09-26 21:30:08 +0800

    Add supabase config file

--- config/supabase.js
+++ config/supabase.js
@@ -1,8 +1,7 @@
 const { createClient } = require("@supabase/supabase-js");
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
-
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const supabase = createClient(supabaseUrl, supabaseServiceKey);
 
 module.exports = supabase;
